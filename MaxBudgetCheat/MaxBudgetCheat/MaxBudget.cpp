36387f3bde5ab5e243a8a6b5d6e2619c7584d840
Studumb <112404846+Studumb@users.noreply.github.com>
2023-08-19 17:33:12 +0700

    Update MaxBudget.cpp

--- MaxBudgetCheat/MaxBudgetCheat/MaxBudget.cpp
+++ MaxBudgetCheat/MaxBudgetCheat/MaxBudget.cpp
@@ -13,10 +13,14 @@ MaxBudget::~MaxBudget()
 
 void MaxBudget::ParseLine(const ArgScript::Line& line)
 {
+	//The cheat will only work if the user is currently in an editor
 	if (Editor.IsActive()) {
+		//There might be some mods that change the maximum budget so we need a large value to add
+		//in order to put the budget value to its maximum value and in this case is MAXIMUM_ALLOWED
 		Editor.mpEditorLimits->AddValue(Editor.mpEditorLimits->kBudget, MAXIMUM_ALLOWED);
 	}
 	else {
+		//If the user isn't in an editor, but they use the cheat, it will print this text line to the cheat console
 		App::ConsolePrintF("Enter an editor to use this cheat.");
 	}
 }
