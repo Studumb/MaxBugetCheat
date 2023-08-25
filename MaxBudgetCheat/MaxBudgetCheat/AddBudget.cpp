b6f71d3f2ce89f2d9000890f4fe86c4e7fdbb3c6
Studumb <112404846+Studumb@users.noreply.github.com>
2023-08-25 11:06:40 +0700

    Update AddBudget.cpp

--- MaxBudgetCheat/MaxBudgetCheat/AddBudget.cpp
+++ MaxBudgetCheat/MaxBudgetCheat/AddBudget.cpp
@@ -10,13 +10,15 @@ AddBudget::~AddBudget()
 {
 }
 
-
 void AddBudget::ParseLine(const ArgScript::Line& line)
 {
 	size_t argPos;
 	auto arg = line.GetArgumentsRange(&argPos, 0, 1);
+	//We have to make sure that the user uses the cheat in an editor
 	if (Editor.IsActive()) {
+		//If the user enter the argument for the cheat
 		if (argPos == 1) {
+			//Ensure that the value is not smaller than 0
 			if (mpFormatParser->ParseInt(arg[0]) > 0) {
 				Editor.mpEditorLimits->AddValue(Editor.mpEditorLimits->kBudget, mpFormatParser->ParseInt(arg[0]));
 			}
@@ -25,10 +27,12 @@ void AddBudget::ParseLine(const ArgScript::Line& line)
 			}
 		}
 		else {
+			//If the user doesn't enter any arguments, we will just add 150 to the budget
 			Editor.mpEditorLimits->AddValue(Editor.mpEditorLimits->kBudget, 150);
 		}
 	}
 	else {
+		//If the user doesn't use the cheat in an editor, this text will be printed
 		App::ConsolePrintF("Enter an editor to use this cheat.");
 	}
 }
